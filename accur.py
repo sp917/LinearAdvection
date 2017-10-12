cc4c4db733d4983c90163a2f667e463340497743
Stuart J. Patching <sp917@imperial.ac.uk>
2017-10-12 16:09:09 +0100

    Working for CNCS

---
+++ accur.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 12 14:07:09 2017
+
+@author: sp917
+"""
+
+#Solve equation using specified method
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = 0.7
+X = 1
+T = 1
+
+DT = [0.0001,0.00025,0.0005,0.00075,0.001,0.0025,0.005,0.0075,0.01,0.025,0.05,0.075,0.1]
+
+
+dx = 0.001
+N = int(X/dx) + 1
+
+x = np.zeros(N)
+for n in range(0,N):
+    x[n] = n*dx
+    
+A = np.zeros(shape = (N,N)) #CNCS
+B = np.zeros(shape = (N,N)) #CNCS
+
+def f(b):
+    b = b%X
+    return    np.exp(-(b-0.1)**2/0.001) #1 - (b<0.1) - (b>0.2) 
+
+AVERR = []
+for dt in DT:
+    
+    c = a*dt/dx
+    M = int(T/dt) + 1
+    u = np.zeros(shape = (M,N))
+    t = np.zeros(M)
+    
+    for m in range(0,M):
+        t[m] = m*dt
+        
+    u[0] = f(x)
+    
+    for n in range(0,N):
+        for k in range(0,N):        
+            A[n][k] = (n==k) + (c/4)*((n == (k-1)%N) - (n == (k+1)%N))
+            B[n][k] = (n==k) - (c/4)*((n == (k-1)%N) - (n == (k+1)%N))
+            
+    AinvB = np.dot(np.linalg.inv(A),B)
+    
+    for m in range(1,M):
+        u[m] = np.dot(AinvB,u[m-1])
+    
+    #Exact solution:
+    
+    exact = np.zeros(shape=(M,N))
+
+    for n in range(0,N):
+        for m in range(0,M):
+            exact[m][n] = f(x[n] - a*t[m])
+            
+    err = np.abs(u - exact)
+    averr = sum(sum(err))/(M*N)
+    AVERR = AVERR + [averr]
+    
+
+
+DT = np.array(DT)
+grad = np.zeros(shape = DT.size)
+grad[0] = 0
+for n in range(1,grad.size):
+    grad[n] = (np.log(AVERR[n]) - np.log(AVERR[n-1]))/(np.log(DT[n]) - np.log(DT[n-1]))
+
+for n in range(0,DT.size):
+    print("%f  %.9f  %.9f" % (DT[n], AVERR[n], grad[n]))
+
+plt.plot(np.log(DT),np.log(AVERR),'ro')
--- linadv.py
+++ linadv.py
@@ -10,8 +10,7 @@ Created on Wed Oct  4 15:19:42 2017
 # On domain [0,infty)x[0,X]
 # With initial condition u(x,0) = f(x)
 # And periodic boundary conditions
-
-# Using the FTBS method u^{m+1}_{n} = u^{m}_{n} - c(u^{m}_{n} - u^{m}_{n-1})
+# Using the FTBS and CTCS methods
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -20,13 +19,13 @@ a = 0.7
 
 # Set ranges
 
-X = 2*np.pi
-T = 5
+X = 1
+T = 1
 
 #increments
 
-dx = 0.01
-dt = 0.01
+dx = 0.001
+dt = 0.001
 
 #Courant number
 
@@ -40,10 +39,15 @@ M = int(T/dt) + 1
 #boundary function
 
 def f(b):
-    return 1 - (b%X>0.5) - (b%X<0.25)
+    b = b%X
+    return    np.exp(-(b-0.1)**2/0.001) #1 - (b<0.1) - (b>0.2) 
 
-u1 = np.zeros(shape = (M,N))
-u2 = np.zeros(shape=(M,N))
+
+CNCS = np.zeros(shape = (M,N))
+FTBS = np.zeros(shape=(M,N))
+BTBS = np.zeros(shape=(M,N))
+CTCS = np.zeros(shape=(M,N))
+
 x = np.zeros(N)
 t = np.zeros(M)
 
@@ -55,37 +59,59 @@ for m in range(0,M):
 
 #initial condition:
 
-u1[0] = f(x)
-u2[0] = f(x)
+CNCS[0] = f(x)
+BTBS[0] = f(x)
+CTCS[0] = f(x)
+FTBS[0] = f(x)
 
-#Define iteration matrices A,B such that u^{n+1} = Au^{n}
 
-A = np.zeros(shape = (N,N)) #FTBS
-B = np.zeros(shape = (N,N)) #CTCS
-C = np.zeros(shape = (N,N)) #FTCS
+#Define iteration matrices e.g. A such that u^{n+1} = Au^{n}
+
+A = np.zeros(shape = (N,N)) #CNCS
+B = np.zeros(shape = (N,N)) #CNCS
+C = np.zeros(shape = (N,N)) #BTBS
+D = np.zeros(shape = (N,N)) #FTCS
+E = np.zeros(shape = (N,N)) #CTCS
+F = np.zeros(shape = (N,N)) #FTBS
 
 
 for n in range(0,N):
-    for k in range(0,N):
-        A[n][k] = (1-c)*(n==k) + c*(n==k+1)
-        B[n][k] = c*(n==k-1) - c*(n==k+1)
-        C[n][k] = (n==k) - 0.5*c*(n==k-1) + 0.5*c*(n==k+1)
-    
-    
-#FTBS:
+    for k in range(0,N):        
+        A[n][k] = (n==k) + (c/4)*((n == (k-1)%N) - (n == (k+1)%N))
+        B[n][k] = (n==k) - (c/4)*((n == (k-1)%N) - (n == (k+1)%N))
+        C[n][k] = (n==k)*(1+c) - c*( n == (k+1)%N)
+        D[n][k] = (n==k%N) - 0.5*c*(n==((k-1)%N)) + 0.5*c*(n==((k+1)%N))
+        E[n][k] = c*(n==((k-1)%N)) - c*(n==((k+1)%N))
+        F[n][k] = (1-c)*((n==k%N)) + c*(n==((k+1)%N))
+
+
+#CNCS:
+
+AinvB = np.dot(np.linalg.inv(A),B)
 
 for m in range(1,M):
-    u1[m] = np.dot(A,u1[m-1])
+    CNCS[m] = np.dot(AinvB,CNCS[m-1])
     
+#BTBS:
+    
+invC = np.linalg.inv(C)
+
+for m in range(1,M):
+    BTBS[m] = np.dot(invC,BTBS[m-1])
+
 #CTCS:
 #Since CTCS is a two-step method we need a one-step method for the first iteration 
 #We use FTCS
 
-u2[1] = np.dot(C,u2[0])
+CTCS[1] = np.dot(D,CTCS[0])
 
 for m in range(2,M):
-    u2[m] = u2[m-2] - np.dot(B,u2[m-1])
+    CTCS[m] = CTCS[m-2] - np.dot(E,CTCS[m-1])
 
+#FTBS:
+
+for m in range(1,M):
+    FTBS[m] = np.dot(F,FTBS[m-1])
     
 #Exact solution:
     
@@ -95,11 +121,32 @@ for n in range(0,N):
     for m in range(0,M):
         exact[m][n] = f(x[n] - a*t[m])
 
-t = 4.9
-m = int(M*t/T)
+#Errors:
+def err(W):
+    return np.abs(exact - W)
 
-plt.axis([0,X,0,1.3])
-plt.plot(x,u1[m])
-plt.plot(x,u2[m])
-plt.plot(x,exact[m])
+P = M*N
+
+def averr(W):
+    return sum(sum(err(W)))/P
+    
+
+tt = T
+m = int((M-1)*tt/T)
+
+plt.axis([0,X,-1.3,1.4])
+plt.plot(x,CNCS[m], label = 'CNCS')
+plt.plot(x,BTBS[m], label = 'BTBS')
+plt.plot(x,CTCS[m], label = 'CTCS')
+plt.plot(x,FTBS[m], label = 'FTBS')
+plt.plot(x,exact[m], label = 'Exact Solution')
+plt.legend()
+plt.title('t = %f' % t[m])
+plt.xlabel('x')
+plt.ylabel('u')
 plt.show()
+
+print("CNCS: %f" % averr(CNCS))
+print("BTBS: %f" % averr(BTBS))
+print("CTCS: %f" % averr(CTCS))
+print("FTBS: %f" % averr(FTBS))
