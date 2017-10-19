70b88c3892d2e662c8b05347cce99fc976094c07
Stuart J. Patching <sp917@imperial.ac.uk>
2017-10-19 17:52:54 +0100

    Error calculation added

--- LinearAdvectionNumericalSolution.py
+++ LinearAdvectionNumericalSolution.py
@@ -22,29 +22,35 @@ class data: #This class provides the necessary data for solving the equation
         self.a = a
         self.dx = dx
         self.dt = dt
-        self.c = a*dt/dx
-        self.N = int(X/dx) + 1
-        self.M = int(T/dt) + 1
-        self.x = v(dx,self.N)
-        self.t = v(dt,self.M)
+        self.c = a*dt/dx #Courant number
+        self.N = int(X/dx) + 1 #number of grid points in x-direction
+        self.M = int(T/dt) + 1 #number of grid points in t-direction
+        self.x = v(dx,self.N) #vector of all possible values of x
+        self.t = v(dt,self.M) #vector of all possible values of t
     def f(self,x): #Initial condition
         x = x%(self.X)
-        return np.exp(-(x-0.1)**2/0.001)# 1 - (x<0.1) - (x>0.2) #
+        return 1 - (x<0.1) - (x>0.2) #np.exp(-(x-0.1)**2/0.001)# 
+    def exactsolution(self):
+        exact = np.zeros(shape=(self.M,self.N))
+        for n in range(0,self.N):
+            for m in range(0,self.M):
+                exact[m][n] = self.f(self.x[n] - self.a*self.t[m])
+        return exact
+    def changedt(self,dtnew):
+        self.dt = dtnew
+        self.c = self.a*self.dt/self.dx
+        self.M = int(self.T/self.dt) + 1
+        self.t = v(self.dt,self.M)
+    def changedx(self,dxnew):
+        self.dx = dxnew
+        self.c = self.a*self.dt/self.dx
+        self.N = int(self.X/self.dx) + 1
+        self.x = v(self.dx,self.N)
     
-
-def exactsolution(y):
-    y.N = int(y.X/y.dx) + 1
-    y.M = int(y.T/y.dt) + 1
-    exact = np.zeros(shape=(y.M,y.N))
-    for n in range(0,y.N):
-        for m in range(0,y.M):
-            exact[m][n] = y.f(y.x[n] - y.a*y.t[m])
-    return exact
-
 
 def C(i,N,c): #C will be the matrix such that u^{n+1} = Cu^{n}
     
-    if i==1: #CNCS
+    if i=='CNCS': 
         A = np.zeros(shape=(N,N))
         B = np.zeros(shape = (N,N)) 
         for n in range(0,N):
@@ -53,19 +59,18 @@ def C(i,N,c): #C will be the matrix such that u^{n+1} = Cu^{n}
                 B[n][k] = (n==k) - (c/4)*((n == (k-1)%N) - (n == (k+1)%N))
         return np.dot(np.linalg.inv(A),B)
     
-    if i==2: #FTBS
+    if i=='FTBS': 
         A = np.zeros(shape = (N,N))
         for n in range(0,N):
                 for k in range(0,N):
                     A[n][k] = (1-c)*((n==k%N)) + c*(n==((k+1)%N))
         return A
-            
 
 #Function solving the linear advection equation using the selected method
     
 def solve(y,i):
-    
-    D = C(i,y.N,y.c)    
+        
+    D = C(i,y.N,y.c)
     u = np.zeros(shape = (y.M,y.N))
     
     #Set initial condition
@@ -80,7 +85,9 @@ def solve(y,i):
     return u
 
 #Define a function to calculate the l1 normed error
-def error(y,u,ex):
+    
+def error(y,u):
+    ex = y.exactsolution()
     er = np.abs(ex - u)
     Tot = sum(sum(u))
     ertot = sum(sum(er))/Tot
@@ -88,27 +95,77 @@ def error(y,u,ex):
 
 #Plot the graph of the solution at specified time and print the total error
 
-def plot(y,tt):
+def plotsol(y,i,u,tt):
     m = int((y.M-1)*tt/y.T)
-    CTCS = solve(y,1)
-    FTBS = solve(y,2)
-    exact = exactsolution(y)
     plt.axis([0,y.X,-1.3,1.4])
-    plt.plot(y.x,CTCS[m], label = 'CNCS')
-    plt.plot(y.x,FTBS[m],label = 'FTBS')
-    plt.plot(y.x,exact[m], label = 'Exact Solution')
+    plt.plot(y.x,u[m],label = i)
     plt.legend()
     plt.title('t = %.3f' % y.t[m])
     plt.xlabel('x')
     plt.ylabel('u')
-    plt.show()
-    errCTCS = error(y,CTCS,exact)
-    errFTBS = error(y,FTBS,exact)
-    print("Error for CNCS: %f" % errCTCS)
-    print("Error for FTBS: %f" % errFTBS)
     
-y = data(X=1,T=1,a=0.7,dx=0.001,dt=0.001)
-plot(y,1)
+#Solve for various values of dx and calculate the errors
+    
+def ploterr(y,i):
+    
+    DX = [0.001,0.0025,0.005,0.0075,0.01,0.025,0.05,0.075,0.1]
+    Errors = []
+    
+    for j in range(0,len(DX)):
+        y.changedx(DX[j])
+        u = solve(y,i)
+        er = error(y,u)
+        Errors += [er]
+ 
+    #We want to plot log(error) graphs
+    
+    Errors = np.array(Errors)
+    Errors = np.log(Errors)
+    DX = np.array(DX)
+    DX = np.log(DX)
+
+    plt.xlabel(r'$\log(\Delta x)$')
+    plt.ylabel(r'$\log(Error)$')
+    plt.plot(DX,Errors,'o',label = i)
+    plt.legend()
+    
+    
+
+def main():
+    
+    y = data(X=1,T=1,a=0.7,dx=0.001,dt=0.001)
+    
+    print("Solving with CNCS")
+    
+    CNCS = solve(y,'CNCS')
+    
+    print("Solved with CNCS")
+    
+    print("Solving with FTBS")
+    
+    FTBS = solve(y,'FTBS')
+    
+    print("Solved with FTBS")
+    
+    print("Calculating exact solution")
+    
+    Exact = y.exactsolution()
+    
+    print("Plotting solutions")
+    
+    plotsol(y, 'CNCS', CNCS, 1)
+    plotsol(y,'FTBS', FTBS, 1)
+    plotsol(y, 'Exact', Exact,1)
+    plt.show()
+    
+    print("Plotting errors")
+    
+    ploterr(y,'FTBS')
+    ploterr(y,'CNCS')
+    
+    plt.show()
+    
+main()
 
 
 
