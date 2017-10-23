849fb06ada0bf3458b79a0148c84582a70c2413e
Stuart J. Patching <sp917@imperial.ac.uk>
2017-10-23 14:02:04 +0100

    some things re-arranged in the code

--- LinearAdvectionNumericalSolution.py
+++ LinearAdvectionNumericalSolution.py
@@ -21,6 +21,24 @@ def gauss(x):
 def step(x):
     return 1 - (x<0.1) - (x>0.2)
 
+def C(i,N,c): #C will be the matrix such that u^{n+1} = Cu^{n}
+    
+    if i=='CNCS': 
+        A = np.zeros(shape=(N,N))
+        B = np.zeros(shape = (N,N)) 
+        for n in range(0,N):
+            for k in range(0,N):        
+                A[n][k] = (n==k) + (c/4)*((n == (k-1)%N) - (n == (k+1)%N))
+                B[n][k] = (n==k) - (c/4)*((n == (k-1)%N) - (n == (k+1)%N))
+        return np.dot(np.linalg.inv(A),B)
+    
+    if i=='FTBS': 
+        A = np.zeros(shape = (N,N))
+        for n in range(0,N):
+                for k in range(0,N):
+                    A[n][k] = (1-c)*((n==k%N)) + c*(n==((k+1)%N))
+        return A
+
 class data: #This class provides the necessary data for solving the equation
     def __init__(self,X,T,a,dx,dt):
         self.X = X
@@ -54,44 +72,21 @@ class data: #This class provides the necessary data for solving the equation
         self.x = v(self.dx,self.N)
     
 
-def C(i,N,c): #C will be the matrix such that u^{n+1} = Cu^{n}
-    
-    if i=='CNCS': 
-        A = np.zeros(shape=(N,N))
-        B = np.zeros(shape = (N,N)) 
-        for n in range(0,N):
-            for k in range(0,N):        
-                A[n][k] = (n==k) + (c/4)*((n == (k-1)%N) - (n == (k+1)%N))
-                B[n][k] = (n==k) - (c/4)*((n == (k-1)%N) - (n == (k+1)%N))
-        return np.dot(np.linalg.inv(A),B)
-    
-    if i=='FTBS': 
-        A = np.zeros(shape = (N,N))
-        for n in range(0,N):
-                for k in range(0,N):
-                    A[n][k] = (1-c)*((n==k%N)) + c*(n==((k+1)%N))
-        return A
-
 #Function solving the linear advection equation using the selected method
-    
 def solve(y,i):
-        
     D = C(i,y.N,y.c)
     u = np.zeros(shape = (y.M,y.N))
     
     #Set initial condition
-    
     u[0] = y.f(y.x)
     
-    #Now we may iterate:
-
+    #Iterate
     for m in range(1,y.M):
         u[m] = np.dot(D,u[m-1])
-        
     return u
 
+
 #Function to calculate the l1 normed error
-    
 def error(y,u):
     ex = y.exactsolution()
     er = np.abs(ex - u)
@@ -99,8 +94,33 @@ def error(y,u):
     ertot = sum(sum(er))/Tot
     return ertot
 
-#Plot the graph of the solution at specified time and print the total error
 
+def dxvalues(L, h):
+    DX = []
+    for k in range(1,L+1):
+        DX += [k*h]
+    DX = np.array(DX)
+    logDX = np.log(DX)
+    return DX, logDX
+
+
+def errorlogerror(y, i, DX):
+    Errors = []
+    
+    for j in range(0,DX.size):
+        y.changedx(DX[j])
+        y.changedt(DX[j]) # this keeps the courant number the same
+        u = solve(y,i)
+        er = error(y,u)
+        Errors += [er]    
+
+    #We want to plot log(error) graphs
+    Errors = np.array(Errors)
+    logErrors = np.log(Errors)
+    return Errors, logErrors
+
+
+#Plot the graph of the solution at specified time and print the total error
 def plotsol(y,i,u,tt):
     m = int((y.M-1)*tt/y.T)
     plt.axis([0,y.X,-1.3,1.4])
@@ -111,45 +131,21 @@ def plotsol(y,i,u,tt):
     plt.xlabel(r'$x$')
     plt.ylabel(r'$u$')
     
+    
 #Output a table of error values
-
-def table(DX,Errors):
+def table(DX, Errors, logDX, logErrors, d2, d1, d0):
     if (DX.size != Errors.size):
         print("Mismatched array size")
     else:
         print('%20s %20s %20s' % ('dx', 'Error', 'Gradient'))
-        logErrors = np.log(Errors)
-        logDX = np.log(DX)
-        d2,d1,d0 = np.polyfit(logDX, logErrors, 2) # quadratic of best fit
-        grad = 2*d2*logDX + d1
+        grad = 2*d2*logDX + d1 #we want the gradient of the log-log graph
         for j in range(0,DX.size):
             s = '\hline\n%20.3f & %20f & %20f' % (DX[j], Errors[j], grad[j])
             print(s)#prints in LaTeX format
-
+     
+        
 #Solve for various values of dx and calculate the errors
-    
-def ploterr(y,i):
-    
-    DX = []
-    for k in range(1,100):
-        DX += [k*0.001]
-    DX += [0.1]
-    Errors = []
-    
-    for j in range(0,len(DX)):
-        y.changedx(DX[j])
-        y.changedt(DX[j]) # this keeps the courant number the same
-        u = solve(y,i)
-        er = error(y,u)
-        Errors += [er]    
-
-    #We want to plot log(error) graphs
-    
-    DX = np.array(DX)
-    Errors = np.array(Errors)
-    logErrors = np.log(Errors)
-    logDX = np.log(DX)
-    d2,d1,d0 = np.polyfit(logDX, logErrors, 2) #quadratic of best fit
+def ploterr(logDX, logErrors, i, d2, d1, d0):
     plt.rc('text', usetex = True)
     plt.xlabel(r'$\log(\Delta x)$')
     plt.ylabel(r'$\log(Error)$')
@@ -157,41 +153,55 @@ def ploterr(y,i):
     plt.plot(logDX, d2*(logDX**2) + d1*(logDX) + d0,label = i)
     plt.legend()
     
-    table(DX,Errors)
+    
+#Solve then plot at the specified time:
+def SOLVING(y, methods): 
+     for i in methods:
+        print("\nRunning %s with dx = %.3f, dt = %.3f, a = %.2f" 
+              % (i,y.dx,y.dt,y.c))
+        u = solve(y,i)
+        print("\nSolved with %s" % i)
+        plotsol(y, i, u, 1)
+
+
+#Solves for various values of dx then plots log(dx) against log(error)
+def ERRORPLOTTING(y, methods, DX, logDX):
+    for i in methods:
+        print("\nTable of errors for %s:" % i)
+        Errors, logErrors = errorlogerror(y,i,DX)
+        d2,d1,d0 = np.polyfit(logDX, logErrors, 2) #quadratic of best fit
+        table(DX, Errors, logDX, logErrors, d2, d1, d0)
+        ploterr(logDX, logErrors, i, d2, d1, d0)
+
 
 def main():
     
     y = data(X=1,T=1,a=0.7,dx=0.001,dt=0.001)
-    
     methods = ['CNCS', 'FTBS']
+    DX,logDX = dxvalues(100,0.001)
     
     print("\nCalculating exact solution")
     
     Exact = y.exactsolution()
     
-    for i in methods:
-        print("\nRunning %s with dx = %.3f, dt = %.3f, a = %.2f" 
-              % (i,y.dx,y.dt,y.c))
-        
-        u = solve(y,i)
+    print("\nPlotting solutions")
     
-        print("\nSolved with %s" % i)
-        plotsol(y, i, u, 1)
+    SOLVING(y,methods)
         
     plotsol(y, 'Exact', Exact,1)
-    print("\nPlotting solutions")
+    
     plt.show()
     
     print("\nCalculating errors")
-    for i in methods:
-        print("\nTable of errors for %s:" % i)
-        ploterr(y,i)
-    
+
+    ERRORPLOTTING(y, methods, DX, logDX)
     
     print("\nPlotting errors for both methods:")
+    
     plt.show()
     
     print("\a")
+    
     
 main()
 
